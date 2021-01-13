da0bab0b85d4ec03b6aad6b2ec0ba9512eeae5d2
Matt Henry <matt.henry@gsa.gov>
2021-01-13 16:55:18 -0500

    initial commit

---
+++ src/Exercise.svelte
@@ -0,0 +1,46 @@
+<script lang="typescript">
+  export let id: String;
+  export let name: String;
+  export let weight: Number;
+
+  let tookBreak: Boolean = false;
+</script>
+
+<form class="exercise">
+  <h3>{name}</h3>
+  <div>
+    <label for={`reps-${id}`}>Reps</label>
+    <input type="number" id={`reps-${id}`} />
+  </div>
+
+  {#if weight}
+    <label for={`weight-${id}`}>Weight</label>
+    <input type="number" id={`weight-${id}`} />
+  {/if}
+
+  <div>
+    <label for={`break-${id}`}>Break?</label>
+    <input type="checkbox" id={`break-${id}`} bind:checked={tookBreak} />
+  </div>
+
+  {#if tookBreak}
+    <div class="reps-after-break">
+      <label for={`reps-after-break-${id}`}>Reps after break</label>
+      <input type="number" id={`reps-after-break-${id}`} />
+    </div>
+  {/if}
+
+  <div>
+    <label for={`notes-${id}`}>Notes</label>
+    <input type="text" id={`notes-${id}`} />
+  </div>
+
+  {#if weight}
+    <div>
+      <label for={`do-more-${id}`}>More weight next time?</label>
+      <input type="checkbox" id={`do-more-${id}`} />
+    </div>
+  {/if}
+
+  <button type="submit">Done</button>
+</form>
---
+++ src/Workout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { v4 as uuidv4 } from 'uuid';
+	import Exercise from './Exercise.svelte';
+
+	export let exercises : []
+	export let workoutName : String
+</script>
+
+<h2>{workoutName}</h2>
+
+<Exercise
+	name='wide-front pull-ups'
+	id=uuidv4()
+	/>
+
+<Exercise
+	name='arnold presses'
+	weight=true
+	id=uuidv4()
+	/>
